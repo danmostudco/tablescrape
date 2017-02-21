b669d48b5738d4973c7f4baa298790b586094c51
Dan Morrison <morrison.danr@gmail.com>
2017-02-20 22:08:55 -0500

    set up indexing system for month and time

--- scrape.py
+++ scrape.py
@@ -10,18 +10,25 @@ from selenium.webdriver.support import expected_conditions as EC # available sin
 
 # TODO: Human Delay
 def humanDelay():
-    sleep(random.randint(1, 30)/10)
+    sleep(random.randint(1, 20)/10)
 
 # Set up web driver as browser
 browser = webdriver.Chrome('./chromedriver')
 # base url for Opentable and restaurants to append
 baseurl = "https://www.opentable.com/"
 
+# Store Inputs for OpenTable
+monthsOfYear = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+timesOfDay = ["12:00 AM", "12:30 AM", "1:00 AM", "1:30 AM", "2:00 AM", "2:30 AM", "3:00 AM", "3:30 AM", "4:00 AM", "4:30 AM", "5:00 AM", "5:30 AM", "6:00 AM", "6:30 AM", "7:00 AM", "7:30 AM", "8:00 AM", "8:30 AM", "9:00 AM", "9:30 AM", "10:00 AM", "10:30 AM", "11:00 AM", "11:30 AM", "12:00 PM", "12:30 PM",  "1:00 PM", "1:30 PM", "2:00 PM", "2:30 PM", "3:00 PM", "3:30 PM", "4:00 PM", "4:30 PM", "5:00 PM", "5:30 PM", "6:00 PM", "6:30 PM",  "7:00 PM", "7:30 PM", "8:00 PM", "8:30 PM", "9:00 PM", "9:30 PM", "10:00 PM", "10:30 PM", "11:00 PM", "11:30 PM"]
+
 # How many is the table for | date | name of month | time of meal
 peopleCount = 6
-dayOfMonth = 19
+dayOfMonth = 22
 month = "February"
 mealTime = "12:30 PM"
+# Get the index of the time and month in case next available of either is needed
+mealTimeIndex = timesOfDay.index(mealTime)
+monthIndex = monthsOfYear.index(month)
 
 # set up restaurantResults dictionary
 globalResults = {"allOptions" : []}
